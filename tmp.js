bf4fa7a844e6c27bd5e523f6eeef23bf7d731638
thehumit <A.Ryzhov25@yandex.ru>
2022-09-19 15:32:59 +0300

    commit was made понедельник, 19 сентября 2022 г. 15:32:59 (MSK)

--- tmp.js
+++ tmp.js
@@ -1,33 +1,37 @@
-const https = require('https')
-const options = {
-  hostname: 'www.bloomberg.com',
-  port: 443,
-  path: '/markets2/api/datastrip/CNYEUR:CUR',
-  method: 'GET',
-  headers: {
-    "authority": "www.bloomberg.com",
-    "accept": '*/*',
-    "accept-language": "en-US,en;q=0.9",
-    "cache-control": "no-cache",
-    "pragma": "no-cache",
-    "sec-ch-ua": "\"Google Chrome\";v=\"105\", \"Not)A;Brand\";v=\"8\", \"Chromium\";v=\"105\"",
-    "sec-ch-ua-mobile": "?0",
-    "sec-ch-ua-platform": "\"Windows\"",
-    "sec-fetch-dest": "document",
-    "sec-fetch-mode": "navigate",
-    "sec-fetch-site": "none",
-    "sec-fetch-user": "?1",
-    "upgrade-insecure-requests": "1",
-    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36"
+function getCurrency(CurrencyName){
+    const https = require('https')
+    const options = {
+      hostname: 'www.bloomberg.com',
+      port: 443,
+      path: '/markets2/api/datastrip/' + CurrencyName,
+      method: 'GET',
+      headers: {
+        "authority": "www.bloomberg.com",
+        "accept": '*/*',
+        "accept-language": "en-US,en;q=0.9",
+        "cache-control": "no-cache",
+        "pragma": "no-cache",
+        "sec-ch-ua": "\"Google Chrome\";v=\"105\", \"Not)A;Brand\";v=\"8\", \"Chromium\";v=\"105\"",
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-platform": "\"Windows\"",
+        "sec-fetch-dest": "document",
+        "sec-fetch-mode": "navigate",
+        "sec-fetch-site": "none",
+        "sec-fetch-user": "?1",
+        "upgrade-insecure-requests": "1",
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36"
+    }
+    }
+    const req = https.request(options, (res) => {
+      console.log(`statusCode: ${res.statusCode}`)
+      res.on('data', (d) => {
+        process.stdout.write(d)
+      })
+    })
+    req.on('error', (error) => {
+      console.error(error)
+    })
+    req.end()
 }
-}
-const req = https.request(options, (res) => {
-  console.log(`statusCode: ${res.statusCode}`)
-  res.on('data', (d) => {
-    process.stdout.write(d)
-  })
-})
-req.on('error', (error) => {
-  console.error(error)
-})
-req.end()
+
+getCurrency("CNYEUR:CUR")
